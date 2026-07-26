d5ab79cbcd1b6f71906b6d630c52b002735e25db
Manbiir Singh <manbiir25@gmail.com>
2026-07-26 15:05:05 +0530

    003-project-new

---
+++ 003-addition-2nos/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c=0;
+    printf("Enter the value of a: \n");
+    scanf("%d",&a);
+    printf("Enter the value of b: \n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum of a and b is: %d",c);  
+    return 0;
+}
